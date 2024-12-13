dc4d52b5c50984e0636877847e52698cff2580b3
GatorSAT <tanyanjas@aol.com>
2024-12-12 20:24:03 -0500

    Update FlatSat_student.py

--- FlatSat_student.py
+++ FlatSat_student.py
@@ -21,7 +21,7 @@ from adafruit_lsm6ds.lsm6dsox import LSM6DSOX as LSM6DS
 from adafruit_lis3mdl import LIS3MDL
 from git import Repo
 from picamera2 import Picamera2
-
+import logging
 
 # VARIABLES
 THRESHOLD = 15
