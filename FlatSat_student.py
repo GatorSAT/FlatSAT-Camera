5fe25f6edbe73a1de7404e6bd29aaa462918ffb2
GatorSAT <tanyanjas@aol.com>
2024-12-14 18:18:24 -0500

    Update FlatSat_student.py

--- FlatSat_student.py
+++ FlatSat_student.py
@@ -44,7 +44,7 @@ def git_push():
     print('added remote')
     origin.pull()
     print('pulled changes')
-    repo.git.add(REPO_PATH + FOLDER_PATH)
+    repo.git.add(REPO_PATH + "/" + FOLDER_PATH)
     repo.index.commit('New Photo')
     print('made the commit')
     origin.push()
